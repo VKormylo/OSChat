06b376c31f383a7850d800f185d7d4d31f47cb38
KapitaniukMax <maxkapitaniuk@gmail.com>
2024-12-06 19:47:51 +0200

    Fixed empty message class

--- logic/Message.cs
+++ logic/Message.cs
@@ -2,5 +2,19 @@ namespace logic;
 
 public class Message
 {
+    public string Text { get; set; } = string.Empty;
+
+    public bool IsReceived { get; set; }
     
+    public DateTime TimeStamp { get; set; }
+    
+    public Message()
+    {}
+
+    public Message(string text, bool isReceived = false)
+    {
+        Text = text;
+        IsReceived = isReceived;
+        TimeStamp = DateTime.Now;
+    }
 }
--- server/obj/Debug/net6.0-windows/App.g.cs
+++ server/obj/Debug/net6.0-windows/App.g.cs
@@ -44,7 +44,7 @@ namespace server {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace server {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             server.App app = new server.App();
             app.InitializeComponent();
