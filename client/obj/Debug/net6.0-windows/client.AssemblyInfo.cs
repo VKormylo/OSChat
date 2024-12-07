0e6ddb06660e9bf50fe4ad3f605e89dc57a50264
Святослав Самолюк <samoliuk.sviatoslav@gmail.com>
2024-12-07 22:07:32 +0200

    Changed SocketsViewModel to WinApi functions

--- client/obj/Debug/net6.0-windows/client.AssemblyInfo.cs
+++ client/obj/Debug/net6.0-windows/client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d25ce6841ef44eadbf12e690029623fed7df88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
