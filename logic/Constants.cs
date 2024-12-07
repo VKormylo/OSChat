6ff03817c56d90ab6dce06729ba7b200c359e7ca
TanyaTverdun <tanyatverdun@gmail.com>
2024-12-07 21:53:18 +0200

    Created MappingViewModel class

---
+++ logic/Constants.cs
@@ -0,0 +1,9 @@
+namespace logic;
+
+public static class Constants
+{
+    public const string FILE_NAME = "FileMapping.txt";
+    public const uint PAGE_READWRITE = 0x40000000;
+    public const uint FILE_SIZE = 1024;
+    public const uint FILE_MAP_ALL_ACCESS = 0x1F0FFF;
+}
