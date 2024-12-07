e8ead38068abd15165bc3f0cc7a3b9ffe8c745fe
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2024-12-07 22:48:45 +0200

    Implemented ability to switch between sync methods

---
+++ client/MVVM/View/SocketsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace client.MVVM.View;
+
+public partial class SocketsView : UserControl
+{
+    public SocketsView()
+    {
+        InitializeComponent();
+    }
+}
