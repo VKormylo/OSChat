1cecdb4c3301040dca592a9393a5d45c052707e5
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2024-12-09 23:48:56 +0200

    Created logout button functionality

--- server/obj/Debug/net6.0-windows/server.AssemblyInfo.cs
+++ server/obj/Debug/net6.0-windows/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07d969c06b54279e940c901e729771368b473d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
