08c780a1ffdb0c6bfdd27d04386d50c1b645f8bc
TanyaTverdun <tanyatverdun@gmail.com>
2024-12-10 17:22:02 +0200

    Fixed scrolling down on message sent or received in file mapping

--- server/obj/Debug/net6.0-windows/server.AssemblyInfo.cs
+++ server/obj/Debug/net6.0-windows/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2dc80c9ba87cfae795ec085b2a5a5c06aeb9c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf0ad68ed1f6f5df8906ecb9c93396c3279c32f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
